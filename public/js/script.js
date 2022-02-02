6331e03f926469b784c48dfaab4468612d329dd0
Leandro Miranda Fahur Machado <leandro@veganofoods.com>
2022-02-02 13:46:41 -0800

    contacts endpoint created | fetchContacts & generateContactsCard functions from script.js

---
+++ public/js/script.js
@@ -0,0 +1,47 @@
+const cards = document.getElementById('cards');
+
+/**
+ *
+ * Anonymous function: fetch the cleaned contact list
+ * from the endpoint: /contacts.
+ *
+ */
+const fetchContacts = async () => {
+  const response = await fetch('http://localhost:3000/contacts', {
+    method: 'GET',
+  });
+
+  const data = await response.json();
+  return data;
+};
+
+/**
+ *
+ * Anonymous function: format the list with the html + css.
+ *
+ */
+const generateContactsCard = async () => {
+  // fetch the contacts using the fetch API:
+  const contacts = await fetchContacts();
+
+  // map through the contacts array and generate the cards:
+  const htmlStr = contacts.map((contact) => {
+    return `<li class='contact-item cf'>
+            <div class='contact-details'>
+              <img class='avatar' src='${contact.src}' />
+              <h3>${contact.name}</h3>
+              <span class='email'>${contact.email}</span>
+            </div>
+            <div class='joined-details'>
+              <span class='date'>${contact.joined}</span>
+            </div>
+          </li>`;
+  });
+
+  // display the cards:
+  cards.innerHTML = htmlStr;
+};
+
+// fetchContacts();
+generateContactsCard();
+// fetchContacts().then((contacts) => console.log(contacts));
--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
-const express = require("express");
-const res = require("express/lib/response");
+const express = require('express');
 const app = express();
 
-app.use(express.json());
-app.use(express.static("public"));
+// contacts:
+const contacts = require('./contacts');
 
-app.get("/", (req, response) => {
-  res.status(200).sendFile("index.html");
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile('index.html');
+});
+
+app.get('/contacts', (req, res) => {
+  res.status(200).send(contacts);
 });
 
 app.listen(3000, () => {
-  console.log("🚀 Server up and running on port 3000");
+  console.log('🚀 Server up and running on port 3000');
 });
